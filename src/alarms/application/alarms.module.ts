78f6a94e5b0c24cf47243f6e47e54e2890338e4d
Todd Ericskon <todd.erickson87@gmail.com>
2025-01-04 15:27:14 -0700

    Finished Experimenting with CQRS Part 2

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -4,10 +4,11 @@ import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmFactory } from '../domain/factories/alarm.factory';
 import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
 import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
+import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
 
 @Module({
   controllers: [AlarmsController],
-  providers: [AlarmsService, AlarmFactory, CreateAlarmCommandHandler, GetAlarmsQueryHandler],
+  providers: [AlarmsService, AlarmFactory, CreateAlarmCommandHandler, GetAlarmsQueryHandler, AlarmCreatedEventHandler],
 })
 export class AlarmsModule {
   static withIfrastructure(infratstructureModule: Type | DynamicModule) {
--- src/alarms/application/commands/create-alarm.command-handler.ts
+++ src/alarms/application/commands/create-alarm.command-handler.ts
@@ -1,8 +1,9 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { CreateAlarmCommand } from './create-alarm.command';
 import { Logger } from '@nestjs/common';
 import { AlarmRepository } from '../ports/alarm.repository';
 import { AlarmFactory } from '../../domain/factories/alarm.factory';
+import { AlarmCreatedEvent } from '../../domain/events/alarm-created.event';
 
 @CommandHandler(CreateAlarmCommand)
 export class CreateAlarmCommandHandler implements ICommandHandler<CreateAlarmCommand> {
@@ -11,12 +12,16 @@ export class CreateAlarmCommandHandler implements ICommandHandler<CreateAlarmCom
   constructor(
     private readonly alarmResposity: AlarmRepository,
     private readonly alarmFactory: AlarmFactory,
+    private readonly eventBus: EventBus,
   ) {}
 
   async execute(command: CreateAlarmCommand): Promise<any> {
     this.logger.debug(`Processing "CreateAlarmCommand": ${JSON.stringify(command)}`);
     const alarm = this.alarmFactory.create(command.name, command.severity);
+    const newAlarm = this.alarmResposity.save(alarm);
 
-    return this.alarmResposity.save(alarm);
+    this.eventBus.publish(new AlarmCreatedEvent(alarm));
+
+    return newAlarm;
   }
 }
